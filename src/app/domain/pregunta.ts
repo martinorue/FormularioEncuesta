8d6bdab9045755fc75a5590ec1f25da5effa62f1
martinorue <martin.orue008@gmail.com>
2022-01-31 20:10:27 -0300

    corrección whiteSpaceValidator

--- src/app/domain/pregunta.ts
+++ src/app/domain/pregunta.ts
@@ -6,27 +6,5 @@ export class Pregunta {
   EncuestaID!: number | undefined;
   Requerida!: boolean;
   Opciones!: { OpcionID: number, OpcionTexto: string, checked: boolean }[];
-  
-  
-
-  // constructor(opciones: {
-  //   value?: string;
-  //   preguntaId?: number;
-  //   textoPregunta?: string;
-  //   requerido?: boolean;
-  //   orden?: number;
-  //   controlType?: string;
-  //   encuestaId?: number;
-  //   opciones?: { OpcionId: number, OpcionTexto: string, checked: boolean }[];
-  // } = {}) {
-  //   this.value = opciones.value || undefined;
-  //   this.PreguntaID = opciones.preguntaId;
-  //   this.TextoPregunta = opciones.textoPregunta || '';
-  //   this.Requerida = !!opciones.requerido;
-  //   this.Orden = opciones.orden === undefined ? 1 : opciones.orden;
-  //   this.Tipo = opciones.controlType || '';
-  //   this.EncuestaID = opciones.encuestaId || undefined;
-  //   this.Opciones = opciones.opciones || [];
-  // }
 }
 
