3b054a5665ff5b19e0a6ae23fbdd82d684cecbda
martinorue <martin.orue008@gmail.com>
2022-02-01 03:13:17 -0300

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "./environment-definition";
+
+export const environment: Environment = {
+  production: true,
+  baseUri: 'https://mr87187.azurewebsites.net',//PROD
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,12 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+import { Environment } from "./environment-definition";
 
+export const environment: Environment = {
+  production: false,
+  baseUri: 'https://mr87187.azurewebsites.net',//DEVELOP
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
