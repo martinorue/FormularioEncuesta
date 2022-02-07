25e68e7b93933b18f16a7daf3966495ce263b0f5
martinorue <martin.orue008@gmail.com>
2022-02-07 19:53:06 -0300

    configuración environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from "./environment-definition";
 
 export const environment: Environment = {
   production: true,
-  baseUri: 'https://formularioencuesta.azurewebsites.net',//PROD
+  baseUri: 'https://sistemaencuestasapi.azurewebsites.net',//PROD
 };
